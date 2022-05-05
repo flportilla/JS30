24d18fe7c5464f6e38676201cdb1c0bab0c24b52
flportilla <flportilla@gmail.com>
2022-05-05 17:20:29 -0500

    get albums

---
+++ getAlbums.js
@@ -0,0 +1,38 @@
+const albumsInfo = document.querySelectorAll(".hyHkMMynp3uUsmEtOkSN")
+const songsInfo = document.querySelectorAll('.koyeY6AgGRPmyPITi7yO + .JUa6JJNj7R_Y3i4P8YUX')
+
+const discography = []
+
+const songs = []
+songsInfo.forEach( (albumContainer, index) =>{
+    const songsContainer = albumContainer.querySelectorAll(`[data-testid="tracklist-row"]`)
+
+    songsContainer.forEach(row =>{
+        const songName = row.querySelector('.t_yrXoUO3qGsJS4Y6iXX')
+        const songsDuration = row.querySelector('.Btg2qHSuepFGBG6X0yEN')
+        if(!songs[index]){
+            songs[index] = []
+        }
+        songs[index].push({
+            name: songName.textContent,
+            duration: songsDuration.textContent
+        })
+    })
+})
+
+albumsInfo.forEach(albumInfo => {
+    const albumName = albumInfo.querySelector('a')
+    const albumYear = albumInfo.querySelector('.Fb61sprjhh75aOITDnsJ span:nth-child(2)')
+    const albumNumberOfSongs = albumInfo.querySelector('.Fb61sprjhh75aOITDnsJ span:nth-child(3)')
+    const stringToNumber = parseInt((albumNumberOfSongs.textContent).trim().split(" ")[0])                  
+
+    discography.push({
+        name: albumName.textContent,
+        year: parseInt(albumYear.textContent),
+        numberOfSongs: stringToNumber,
+        songs: '',
+
+    })
+})
+console.log(albumsInfo)
+console.log(songsInfo)
